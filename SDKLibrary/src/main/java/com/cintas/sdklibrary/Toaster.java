40e6999c78371deae8c358df3a38607e49126bcf
Ajith_S <s.ajith@rifluxyss.com>
2022-08-10 19:48:15 +0530

    Initial commit

---
+++ SDKLibrary/src/main/java/com/cintas/sdklibrary/Toaster.java
@@ -0,0 +1,11 @@
+package com.cintas.sdklibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void showToast(Context context, String message){
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+}
